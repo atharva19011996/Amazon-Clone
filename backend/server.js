cde81bb234844b08b13eeafdc8f2b324eca18bd1
Atharva Munshi <atharvachaitanya.munshi@sjsu.edu>
2020-07-18 23:01:51 -0700

    connected to mongoDB

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,24 @@
 import express from "express";
 import data from "./data";
+import dotenv from "dotenv";
+import config from "./config";
+import mongoose from "mongoose";
+import userRoute from "./routes/userRoute";
+
+dotenv.config();
+
+const mongodbUrl = config.MONGODB_URL;
+mongoose
+  .connect(mongodbUrl, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+  })
+  .catch((error) => console.log(error.reason));
 
 const app = express();
 
+app.use("/api/users", userRoute);
 app.get("/api/products/:id", (req, res) => {
   const productId = req.params.id;
   const product = data.products.find((x) => x._id === productId);
@@ -12,7 +28,6 @@ app.get("/api/products/:id", (req, res) => {
 app.get("/api/products", (req, res) => {
   res.send(data.products);
 });
-
 app.listen(5000, () => {
   console.log("Server started at http://localhost:5000");
 });
